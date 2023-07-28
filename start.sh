daafa9fb94bec75c71b4b889ed28c18b882e1b4b
Aaron Scott <aaron@aaronmbp.local>
2023-07-28 13:33:34 +1000

    Additional gunicorn workers for performance

--- start.sh
+++ start.sh
@@ -17,7 +17,7 @@ set -m
   
 # Start the primary process and put it in the background:
 # starting the gunicorn server for the API proxy
-gunicorn --bind 0.0.0.0:5000 wsgi:app &
+gunicorn --bind 0.0.0.0:5000 --workers 2 --threads=4 --worker-class=gthread --worker-tmp-dir /dev/shm wsgi:app &
 
 # Start the helper process and put it in the foreground:
 # starts the Apache server for the UI/ frontend
